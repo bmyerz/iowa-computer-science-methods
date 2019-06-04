ad7a87dbf0a23cb98caf99e9d97e5820d1b701ff
Brandon Myers <bmyers1788@gmail.com>
2019-06-03 23:46:11 -0500

    Create blinkled.py

---
+++ arduino/blinkled.py
@@ -0,0 +1,21 @@
+from arduino import arduino
+import time
+
+# change COM5 to the serial port that Arduino IDE shows
+board = arduino.Arduino('COM5')
+
+# tell the board that we will use digital pin 2 as an output
+board.output([2])
+
+# digital pins are binary: they can only have two values: HIGH or LOW
+# set pin 2 to high
+board.setHigh(2)
+
+# wait one second so that we can see the LED light up
+time.sleep(1)
+
+# set pin 2 to low
+board.setLow(2)
+
+# release control of the board (end of the program)
+board.close()
